38b26701b59d684ba46b8048d7c7d3f396f6f8e4
SumitAchaju <achajusumit@gmail.com>
2023-07-09 13:06:56 +0545

    aspect-ratio applied to image instead of height

--- src/Components/swipper.js
+++ src/Components/swipper.js
@@ -9,13 +9,15 @@ import "swiper/css/navigation";
 export default function Slider(props) {
   const baseUrlImg = "https://sumitachaju.pythonanywhere.com";
   const [height, setHeight] = useState();
+  const [aspectRatio, setAspectRatio] = useState();
   let img = new Image();
   img.onload = () => {
     let imgheight = img.height;
     let imgwidth = img.width;
     let aspectratio = imgwidth / imgheight;
-    let height = document.querySelector(".content").offsetWidth / aspectratio;
-    setHeight(height);
+    // let height = document.querySelector(".content").offsetWidth / aspectratio;
+    // setHeight(height);
+    setAspectRatio(aspectratio);
   };
   img.src = baseUrlImg + props.images[0].image;
   return (
@@ -30,8 +32,8 @@ export default function Slider(props) {
       {props.images.map((image) => (
         <SwiperSlide key={image.id}>
           <img
-            style={{ objectFit: "cover" }}
-            height={`${height}px`}
+            style={{ objectFit: "cover",aspectRatio:aspectRatio  }}
+            // height={`${height}px`}
             src={baseUrlImg + image.image}
             alt=""
           />
